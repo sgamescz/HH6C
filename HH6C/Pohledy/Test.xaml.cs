bb19dc6cda347b9531f29b296919e6cebd750b1a
Aleš Krátký <alesk@aswsyst.cz>
2019-12-30 16:18:37 +0100

    First commit
    First upload

---
+++ HH6C/Pohledy/Test.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+
+namespace WpfApp6.Pohledy
+{
+    /// <summary>
+    /// Interakční logika pro Test.xaml
+    /// </summary>
+    public partial class Test : UserControl
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+
+
+    
+
+
+    }
+
+    public class BindingProxy : Freezable
+    {
+        // Using a DependencyProperty as the backing store for Data. This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+
+        public object Data
+        {
+            get { return (object)this.GetValue(DataProperty); }
+            set { this.SetValue(DataProperty, value); }
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+    }
+
+
+}
